b09bea2b6706a23ecdb262b83278152ad88c0822
eduarda.silva <eduarda.silva@PCLAB14>
2025-03-06 15:05:09 -0300

    Adicionar arquivos de projeto.

---
+++ Eleicao/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Eleicao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpainel = new System.Windows.Forms.Label();
+            this.btnc1 = new System.Windows.Forms.Button();
+            this.btnc2 = new System.Windows.Forms.Button();
+            this.btnc3 = new System.Windows.Forms.Button();
+            this.btnc4 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblResul1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpainel
+            // 
+            this.txtpainel.AutoSize = true;
+            this.txtpainel.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpainel.Location = new System.Drawing.Point(56, 9);
+            this.txtpainel.Name = "txtpainel";
+            this.txtpainel.Size = new System.Drawing.Size(682, 51);
+            this.txtpainel.TabIndex = 0;
+            this.txtpainel.Text = "Eleição Para o Melhor Rei das Fadas";
+            this.txtpainel.Click += new System.EventHandler(this.txtpainel_Click);
+            // 
+            // btnc1
+            // 
+            this.btnc1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnc1.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc1.Location = new System.Drawing.Point(65, 222);
+            this.btnc1.Name = "btnc1";
+            this.btnc1.Size = new System.Drawing.Size(103, 43);
+            this.btnc1.TabIndex = 1;
+            this.btnc1.Text = "Gloxínia";
+            this.btnc1.UseVisualStyleBackColor = false;
+            this.btnc1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnc2
+            // 
+            this.btnc2.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc2.Location = new System.Drawing.Point(253, 222);
+            this.btnc2.Name = "btnc2";
+            this.btnc2.Size = new System.Drawing.Size(103, 40);
+            this.btnc2.TabIndex = 2;
+            this.btnc2.Text = "dália";
+            this.btnc2.UseVisualStyleBackColor = true;
+            this.btnc2.Click += new System.EventHandler(this.btnc2_Click);
+            // 
+            // btnc3
+            // 
+            this.btnc3.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc3.Location = new System.Drawing.Point(434, 222);
+            this.btnc3.Name = "btnc3";
+            this.btnc3.Size = new System.Drawing.Size(111, 43);
+            this.btnc3.TabIndex = 3;
+            this.btnc3.Text = "Harlequin";
+            this.btnc3.UseVisualStyleBackColor = true;
+            this.btnc3.Click += new System.EventHandler(this.btnc3_Click);
+            // 
+            // btnc4
+            // 
+            this.btnc4.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc4.Location = new System.Drawing.Point(635, 225);
+            this.btnc4.Name = "btnc4";
+            this.btnc4.Size = new System.Drawing.Size(103, 40);
+            this.btnc4.TabIndex = 4;
+            this.btnc4.Text = "Nasiens ";
+            this.btnc4.UseVisualStyleBackColor = true;
+            this.btnc4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Eleicao.Properties.Resources.c4;
+            this.pictureBox4.Location = new System.Drawing.Point(623, 84);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(125, 129);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Eleicao.Properties.Resources.d;
+            this.pictureBox3.Location = new System.Drawing.Point(235, 84);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(133, 122);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Eleicao.Properties.Resources.c33;
+            this.pictureBox2.Location = new System.Drawing.Point(434, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(125, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Eleicao.Properties.Resources.g;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblResul1
+            // 
+            this.lblResul1.AutoSize = true;
+            this.lblResul1.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResul1.Location = new System.Drawing.Point(61, 297);
+            this.lblResul1.Name = "lblResul1";
+            this.lblResul1.Size = new System.Drawing.Size(15, 24);
+            this.lblResul1.TabIndex = 10;
+            this.lblResul1.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(169)))), ((int)(((byte)(221)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResul1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnc4);
+            this.Controls.Add(this.btnc3);
+            this.Controls.Add(this.btnc2);
+            this.Controls.Add(this.btnc1);
+            this.Controls.Add(this.txtpainel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtpainel;
+        private System.Windows.Forms.Button btnc1;
+        private System.Windows.Forms.Button btnc2;
+        private System.Windows.Forms.Button btnc3;
+        private System.Windows.Forms.Button btnc4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblResul1;
+    }
+}
+
---
+++ Eleicao/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eleicao
+{
+    public partial class Form1: Form
+    {
+
+        // Variáveis para armazenar os votos dos participantes
+        private int votosParticipante1 = 0;
+        private int votosParticipante2 = 0;
+        private int votosParticipante3 = 0;
+        private int votosParticipante4 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpainel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Evento para votar no Participante 1 (button1)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            votosParticipante1++;
+            AtualizarVotos();
+        }
+
+        // Evento para votar no Participante 2 (btnc2)
+        private void btnc2_Click(object sender, EventArgs e)
+        {
+            votosParticipante2++;
+            AtualizarVotos();
+        }
+
+        // Evento para votar no Participante 3 (btnc3)
+        private void btnc3_Click(object sender, EventArgs e)
+        {
+            votosParticipante3++;
+            AtualizarVotos();
+        }
+
+        // Evento para votar no Participante 4 (button4)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            votosParticipante4++;
+            AtualizarVotos();
+        }
+
+        private void AtualizarVotos()
+        {
+            // Atualiza as contagens de votos no rótulo
+            lblResul1.Text = "\nGloxínia: " + votosParticipante1.ToString();
+            lblResul1.Text = "\nDália: " + votosParticipante2.ToString();
+            lblResul1.Text += "\nHarlequin: " + votosParticipante3.ToString();
+            lblResul1.Text += "\nNasiens: " + votosParticipante4.ToString();
+        }
+
+    }
+}
